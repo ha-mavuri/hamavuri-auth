3f165863cab44934d49454278ca4adf5c9bf4a14
Goutham Mavuri <goutham2728@gmail.com>
2020-04-24 19:08:07 +0530

    Commit Chnages

---
+++ hamavuri.authorizationserver/Extensions/Authentication/CookieAuthenticationExtension.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hamavuri.authorizationserver
+{
+    public static class CookieAuthenticationExtension
+    {
+        public static AuthenticationBuilder AddCookieAuthentication(this AuthenticationBuilder authenticationBuilder)
+        {
+            authenticationBuilder
+                    .AddCookie(CookieAuthenticationScheme.Internal, options =>
+                    {
+                        options.SlidingExpiration = true;
+                        options.ExpireTimeSpan = new TimeSpan(1000 * 1000 * 60 * 20);//20mins
+                        options.Cookie.Name = "haCookie";
+                        options.Cookie.IsEssential = true;
+                        options.Cookie.SameSite = SameSiteMode.None;
+
+                        options.LoginPath = new PathString("/Account/Login");
+                        options.LogoutPath = new PathString("/Account/Logout");
+                    })
+                    .AddCookie(CookieAuthenticationScheme.External, options =>
+                    {
+                        options.Cookie.Name = CookieAuthenticationScheme.External;
+                        options.Cookie.IsEssential = true;
+                        options.Cookie.SameSite = SameSiteMode.None;
+                    });
+            return authenticationBuilder;
+        }
+    }
+}
---
+++ hamavuri.authorizationserver/Extensions/AuthenticationScheme.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hamavuri.authorizationserver
+{
+    public class CookieAuthenticationScheme
+    {
+        public const string Internal = "hamavuri";
+        public const string External = "hamvuri.external";
+    }
+}
